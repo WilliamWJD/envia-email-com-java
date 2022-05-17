6a30a17e623d424adaeb3e93338688d426ce490f
WilliamWJD <willia.jose.wjd@gmail.com>
2022-05-17 08:42:59 -0300

    criando template html para o e-mail

--- src/main/java/enviando/email/App.java
+++ src/main/java/enviando/email/App.java
@@ -6,8 +6,17 @@ package enviando.email;
  */
 public class App {
 	public static void main(String[] args) {
+		StringBuilder stringBuilderTextoEmail = new StringBuilder();
+
+		stringBuilderTextoEmail.append("<strong>Olá,</strong>");
+		stringBuilderTextoEmail.append("<strong>Você está recebendo o acesso ao curso Java</strong>");
+		stringBuilderTextoEmail.append("<p>Estámos muito feliz em tê-lo conosco, vai ser uma jornada e tanto.</p>");
+		stringBuilderTextoEmail.append("<p>Para continuarmos basta clicar no botão abaixo.</p>");
+		stringBuilderTextoEmail.append(
+				"<a target=\"_blank\" href=\"https://projetojavaweb.com/certificado-aluno/plataforma-curso/aulaatual/470389405/idcurso/1/idvideoaula/428\" style=\"padding: 10px; background-color: #7159c1; color: #fff; border:none; text-decoration: none\">Vamos começar ?</a>");
+
 		ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail("william.jose.wjd@gmail.com", "William Dias - Dev Backend",
-				"Teste", "Esse e-mail é um teste");
+				"Teste", stringBuilderTextoEmail.toString());
 
 		enviaEmail.enviarEmail(true);
 	}
